46b01c49eca3a7f93b197eb25fe7961ddd62eb22
Ing. Fonnck <jfonseca.m@hotmail.com>
2021-09-22 07:42:24 -0500

    Log_in App first commit, wish me luck

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assetss/css/App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div className="App">
+      <br />
+      <button className="btn btn-primary">HEY WORLD!</button>
+    </div>
+  );
+}
+
+export default App;
