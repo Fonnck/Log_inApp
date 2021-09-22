f63aa890a5e28807ebab72361f02b72088766b34
Ing. Fonnck <jfonseca.m@hotmail.com>
2021-09-22 11:31:10 -0500

    Intento fallido de POST en el Login

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 import '../assetss/css/LoginPage.css';
+import { ApiUrl } from '../services/api';
+import axios from 'axios';
 
 
 class LoginPage extends React.Component {
+
+    state = {
+        loginForm: {
+            "username": "",
+            "password": ""
+        },
+        error: false,
+        errorMsg: ""
+    }
+
+    changeManager = async e => {
+        await this.setState({
+            loginForm: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+        // console.log(this.state.loginForm);
+    }
+
+    submitManager = e => {
+        e.preventDefault();
+    }
+
+    loginBtnManager = () => {
+
+        axios.post(ApiUrl, this.state.loginForm)
+        .then( rps => {
+            console.log(rps);
+        })
+
+    } 
+
 
     render() {
         return (
@@ -14,21 +49,21 @@ class LoginPage extends React.Component {
                     <div id="formContent">
 
                         <div className="fadeIn first">
-<br/>
+                            <br />
                             <img src="https://freepikpsd.com/media/2019/10/flat-user-icon-png-4-Transparent-Images.png" id="icon" alt="User Icon" />
-<br/>
+                            <br />
                         </div>
 
-                        <form>
-                            <input type="text" id="login" className="fadeIn second" name="username" placeholder="UserName" />
-                            <input type="text" id="password" className="fadeIn third" name="password" placeholder="Password" />
-                            <input type="submit" className="fadeIn fourth" value="Log In" />
+                        <form onSubmit={this.submitManager}>
+                            <input type="text" className="fadeIn second" name="username" placeholder="UserName" onChange={this.changeManager} />
+                            <input type="password" className="fadeIn third" name="password" placeholder="Password" onChange={this.changeManager} />
+                            <input type="submit" className="fadeIn fourth" value="Log In" onclick={this.loginBtnManager} />
                         </form>
 
                         <div id="formFooter">
                             <a className="underlineHover" href="../components/LoginPage.jsx">Forgot Password?</a>
                         </div>
- 
+
                     </div>
 
                 </div>
